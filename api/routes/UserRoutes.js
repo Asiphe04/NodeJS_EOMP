22b97bcf98a4a8b918c2a814b645f2d28d62123e
Asiphe04 <asiphendimlana40@gmail.com>
2023-08-21 12:15:53 +0200

    fixing user routes

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -5,19 +5,19 @@ const controller = require("../controllers/users");
 router.get("/users", (req, res) => {
   controller.getUsers(req, res);
 });
-router.post("/", (req, res) => {
+router.post("/register", (req, res) => {
   controller.register(req, res);
 });
 router.post("/login", (req, res) => {
   controller.login(req, res);
 });
-router.get("/:id", (req, res) => {
+router.get("/users/:id", (req, res) => {
   controller.getUser(req, res);
 });
-router.patch("/:id", (req, res) => {
+router.patch("/users/:id", (req, res) => {
   controller.updateUser(req, res);
 });
-router.delete("/:id", (req, res) => {
+router.delete("/users/:id", (req, res) => {
   controller.deleteUser(req, res);
 });
 
