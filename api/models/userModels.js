7b0d87475d8f3e6bf801f1c3445c5a9002930386
Asiphe04 <asiphendimlana40@gmail.com>
2023-08-21 12:13:44 +0200

    busy with users

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,52 +1,58 @@
-
-const con = require('../config/db')
-const {hash, compare, hashSync} = require('bcrypt');
-const userAuth = require('../middleware/AuthenticateUser');
+const con = require("../config/db");
+const { hash, compare, hashSync } = require("bcrypt");
+const userAuth = require("../middleware/AuthenticateUser");
 
 class User {
   getUsers(req, res) {
-    con.query("SELECT * FROM users", (err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
+    con.query(
+      "SELECT userID, firstName, userAge, Gender, userRole, emailAdd, userPass, userProfile FROM users",
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
   }
   getUser(req, res) {
-    con.query("SELECT * FROM users WHERE userID = ?;", [req.params.id], (err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
+    con.query(
+      "SELECT userID, firstName, userAge, Gender, userRole, emailAdd, userPass, userProfile FROM users WHERE userID = ?;",
+      [req.params.id],
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
   }
   async createUser(req, res) {
     const userInfo = req.body;
-    userInfo.pass = await hash(userInfo.pass, 15)
+    userInfo.pass = await hash(userInfo.pass, 15);
     con.query("INSERT INTO users SET ?;", [userInfo], (err) => {
       if (err) {
-        res.status(401)
+        res.status(401);
         res.json({ err });
       } else {
-        res.status(200)
-        res.send({msg: "New user created"})
+        res.status(200);
+        res.send({ msg: "New user created" });
       }
-    })
+    });
   }
   async login(req, res) {
-    const email = req.body.email
-    const pass = req.body.pass
+    const email = req.body.emailAdd;
+    const pass = req.body.userPass;
 
     con.query(
-      `SELECT firstName, lastName, email, pass, userRole FROM users WHERE email = '${email}';`,
+      `SELECT firstName, userAge, Gender, userRole, emailAdd, userPass, userProfile FROM users WHERE emailAdd = '${email}';`,
       async (err, loginData) => {
         if (err) throw err;
         if (loginData == null || !loginData.length) {
-          res.status(401)
-          res.json({err: "Incorrect email."});
+          res.status(401);
+          res.json({ err: "Incorrect email." });
         } else {
           compare(pass, loginData[0].pass, (err, result) => {
             if (err) throw err;
             const jwToken = userAuth.createToken({
               email,
               pass,
-            })
+            });
             res.cookie("realUser", jwToken, {
               maxAge: 3600000,
               httpOnly: true,
@@ -59,30 +65,34 @@ class User {
               });
             } else {
               res.status(401).json({
-                err: "The password you have entered is not correct or this account has not been registered.",
+                err: "Email or password incorrect.",
               });
             }
           });
-        };
+        }
       }
-    )
-  };
-  async updateUser(req, res) {
-    const userInfo = req.body
-    if (userInfo.pass) {
-      userInfo.pass = await hash(userInfo.pass, 15)
-    }
-    con.query('UPDATE users SET ? WHERE userID = ?;', [userInfo, req.params.id], (err) => {
-      if (err) throw err;
-      res.send([req.body, {msg: "User updated successfully"}])
-    })
-  };
-  deleteUser(req, res) {
-    con.query('DELETE FROM users WHERE userID = ?;', [req.params.id], (err) => {
-      if (err) throw err;
-      res.send([req.body, {msg: "User Deleted Successfully"}])
-    })
+    );
   }
-};
+  async updateUser(req, res) {
+    const userInfo = req.body;
+    if (userInfo.pass) {
+      userInfo.pass = await hash(userInfo.pass, 15);
+    }
+    con.query(
+      "UPDATE users SET ? WHERE userID = ?;",
+      [userInfo, req.params.id],
+      (err) => {
+        if (err) throw err;
+        res.send([req.body, { msg: "User updated successfully" }]);
+      }
+    );
+  }
+  deleteUser(req, res) {
+    con.query("DELETE FROM users WHERE userID = ?;", [req.params.id], (err) => {
+      if (err) throw err;
+      res.send([req.body, { msg: "User Deleted Successfully" }]);
+    });
+  }
+}
 
 module.exports = User;
--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/users');
+const controller = require("../controllers/users");
 
-router.get('/', (req, res) => {
-  controller.getUsers(req, res)
+router.get("/users", (req, res) => {
+  controller.getUsers(req, res);
 });
-router.post('/', (req, res) => {
-  controller.register(req, res)
+router.post("/", (req, res) => {
+  controller.register(req, res);
 });
-router.post('/login', (req, res) => {
-  controller.login(req, res)
+router.post("/login", (req, res) => {
+  controller.login(req, res);
 });
-router.get('/:id', (req, res) => {
-  controller.getUser(req, res)
+router.get("/:id", (req, res) => {
+  controller.getUser(req, res);
 });
-router.patch('/:id', (req, res) => {
-  controller.updateUser(req, res)
+router.patch("/:id", (req, res) => {
+  controller.updateUser(req, res);
 });
-router.delete('/:id', (req, res) => {
-  controller.deleteUser(req, res)
+router.delete("/:id", (req, res) => {
+  controller.deleteUser(req, res);
 });
 
 module.exports = router;
