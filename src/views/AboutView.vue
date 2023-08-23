877c97136bcc81606b9f57c92410cd3496a1b310
Mufuniwa8 <mufuniwambedzi@gmail.com>
2023-08-23 10:56:14 +0200

    first push of the day

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,13 +8,15 @@
       </p>
 
     </div>
-    <div class="col-6">
+    <div class="col-6 brand-container d-flex flex-column">
+      <h2 class="brands">Working with one of the best brands in the Globe</h2>
+      <div class="d-flex justify-content-between b-log">
+        <img class="brand-img" src="https://i.postimg.cc/fW5mscgb/HM.png" alt="">
+        <img class="brand-img" src="https://i.postimg.cc/YqYJsSSM/download.png" alt="">
+        <img class="brand-img" src="https://i.postimg.cc/Vv6VgjNk/the-north-face-brand-logo-white-symbol-clothes-design-icon-abstract-illustration-with-black-backgrou.jpg" alt="">
+        <img class="brand-img" src="https://i.postimg.cc/DfXv7nzY/fcce4567-f070-42b4-9b00-d46c32a9cca8.jpg" alt="">
 
-<h2 class="brands">Working with one of the best brands in the Globe</h2>
-      <img class="brand-img" src="https://i.postimg.cc/fW5mscgb/HM.png" alt="">
-      <img class="brand-img" src="https://i.postimg.cc/YqYJsSSM/download.png" alt="">
-      <img class="brand-img" src="https://i.postimg.cc/Vv6VgjNk/the-north-face-brand-logo-white-symbol-clothes-design-icon-abstract-illustration-with-black-backgrou.jpg" alt="">
-      <img class="brand-img" src="https://i.postimg.cc/DfXv7nzY/fcce4567-f070-42b4-9b00-d46c32a9cca8.jpg" alt="">
+      </div>
     </div>
   
 </div>
@@ -52,6 +54,13 @@
   display: grid;
   grid-template-columns: auto auto;
 }
+.brand-container {
+  /* border: 2px solid white; */
+  margin-top: 90px;
+  width: 600px;
+  height: 400px;
+  display: flex;
+}
 .img-container {
   display: flex;
   margin: 20px;
@@ -74,9 +83,12 @@
   font-family: 'Libre Baskerville', serif;
 }
 .brand-img {
+  border: 2px solid white;
   border-radius: 100px;
   width: 100px;
   height: 100px;
+  display: flex;
+  grid-template-columns: auto auto auto auto;
   object-fit: cover;
   margin: 10px;
 }
@@ -92,19 +104,96 @@
   color: white;
   font-size: 26px;
   font-family: 'Libre Baskerville', serif;
-
 }
-
-
-@media only screen and (max-width: 700px) {
+@media only screen and (max-width: 300px) {
+  html,body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+ }
+ .heading {
+  color: white;
+  font-size: 30px;
+  width: 270px;
+  /* text-align: center; */
+  font-family: 'Libre Baskerville', serif;
+}
   .about-grid{
     margin: 5px;
+    margin-right: 50px;
+    /* border: 2px solid white; */
+    /* width: 250px; */
   }
+  .about-text {
+  color: white;
+  margin-right: 50px;
+  width: 270px;
+  font-size: 15px !important;
+  font-family: 'Libre Baskerville', serif;
+}
+.card-img {
+  margin: 5px;
+  margin-left: 10px;
+  height: 200px;
+  width: 141px !important;  
+  border-radius: 40px;
+  background-color: rgb(33, 33, 80);
+  color: white;
+}
+.brands {
+  /* border: 2px solid white; */
+  margin-left: 40px;
+  color: white;
+  display: grid;
+  /* grid-template-columns: auto; */
+  font-size: 30px !important;
+  width: 200px;
+  font-family: 'Libre Baskerville', serif;
+}
+.brand-img {
+  border-radius: 100px;
+  width: 50px !important;
+  height: 50px !important ;
+  display: flex !important;
+  margin-right: 50px !important;
+  /* flex-template-columns: auto auto !important; */
+  object-fit: cover;
+  margin: 10px;
+}
+.b-log {
+  justify-content: center;
+  width: 10px !important;
+  justify-items: center0;
+  display: flex !important;
+  border: 2px solid white;
+}
+.img-container {
+  display: flex;
+  margin: 20px;
+  justify-content: center;
+}
+}
+
+@media only screen and (max-width: 700px) {
+
+ 
+  .about-grid{
+    margin: 5px;
+    margin-right: 50px;
+  }
+  .about-text {
+  color: white;
+  margin-right: 50px;
+  font-size: 26px;
+  font-family: 'Libre Baskerville', serif;
+}
   .card-img {
   margin: 5px;
   margin-left: 10px;
   height: 400px;
-  width: 300px;
+  width: 241px !important;  
   border-radius: 40px;
   background-color: rgb(33, 33, 80);
   color: white;
@@ -114,6 +203,29 @@
   height: 300px;
   object-fit: cover;
   border-radius: 40px;
+  margin: 10px;
+}
+.container {
+  display: grid;
+  grid-template-columns: auto;
+}
+.brands {
+  /* border: 2px solid white; */
+  margin-right: 50px;
+  color: white;
+  display: grid;
+  grid-template-columns: auto;
+  font-size: 40px;
+  font-family: 'Libre Baskerville', serif;
+}
+.brand-img {
+  border-radius: 100px;
+  width: 100px;
+  height: 100px;
+  display: grid;
+  margin-right: 50px !important;
+  grid-template-columns: auto auto auto auto;
+  object-fit: cover;
   margin: 10px;
 }
 }
