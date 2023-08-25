0754a034fe692a58fc56add5f005a4a2c39844b7
Asiphe04 <asiphendimlana40@gmail.com>
2023-08-25 04:56:30 +0200

    firebase deploying again

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -6,26 +6,7 @@
     <p class="text-white p-1">
       Copyright &copy; 2023 All Rights Reserved by Elegance Scent
     </p>
-    <div class="container">
-      <div class="row">
-        <div class="col-6">
-          <a href="Life Choices, 314 Imam Haron Rd, Lansdowne, Cape Town, 7780"
-            >314 Imam Haron Rd, Lansdowne, Cape Town, 7780</a
-          ><br />
-          <a href="">elegancescent@gmail.com</a><br />
-          <a href="0812855482">081 285 5482</a>
-        </div>
-        <div class="col-6">
-          <i class="icons fa-brands fa-instagram" style="color: #ffffff"></i>
-          <i class="icons fa-brands fa-linkedin" style="color: #ffffff"></i>
-          <i class="icons fa-brands fa-x-twitter" style="color: #ffffff"></i>
-          <i
-            class="icons fa-brands fa-square-facebook"
-            style="color: #ffffff"
-          ></i>
-        </div>
-      </div>
-    </div>
+ 
   </footer>
 </template>
 
@@ -42,17 +23,11 @@ a {
     rgba(21, 22, 25, 1),
     rgba(217, 217, 217, 0)
   );
-  height: 23vh;
+  height: 15vh;
   font-family: "Libre Baskerville", serif;
   font-size: large;
   color: white;
 }
 
-.icons {
-  margin: 5px;
-  /* margin-top: 50px; */
-  width: 50px;
-  /* height: 50px; */
-  font-size: 35px;
-}
+
 </style>
